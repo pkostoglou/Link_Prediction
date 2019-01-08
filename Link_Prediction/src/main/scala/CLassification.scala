f5512969ba76b988f08abe8c8d4de3d45249008f
Sotiris Tap <sotosotirakis@gmail.com>
2019-01-08 21:50:49 +0200

    update

--- Link_Prediction/src/main/scala/CLassification.scala
+++ Link_Prediction/src/main/scala/CLassification.scala
@@ -11,7 +11,12 @@ import org.apache.spark.sql.functions.{split,udf,concat_ws}
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.classification.DecisionTreeClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.mllib
+import org.apache.spark.ml.regression.DecisionTreeRegressionModel
+import org.apache.spark.ml.regression.DecisionTreeRegressor
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.classification.LinearSVC
+import org.apache.spark.ml.feature.MinMaxScaler
+
 
 // For implicit conversions like converting RDDs to DataFrames
 
@@ -39,7 +44,19 @@ object Classification {
     return 0
   }
 
-    //get a dataframe and use tf idf at a specific column
+  def sameJournal(Journal1: String,Journal2: String):Int={
+    if(Journal1==null || Journal2 ==null)
+      return 0
+
+    if(Journal1.compareTo(Journal2)==0){
+      return 1
+    }
+
+    return 0
+  }
+
+
+  //get a dataframe and use tf idf at a specific column
   def TFIDF(x: DataFrame,column: String,columnOut: String,numFeatures: Int):DataFrame = {
     val tokenizer = new Tokenizer().setInputCol(column).setOutputCol("words")
     val wordsData = tokenizer.transform(x.na.fill(Map(column -> "")))
@@ -73,10 +90,10 @@ object Classification {
     import sparkSession.sql
 
     //load node information
-    val dfHeaderless = sparkSession.read.format("csv").option("header","false").load("/home/panos/IdeaProjects/Link_Prediction/Data-20181224/node_information.csv")
+    val dfHeaderless = sparkSession.read.format("csv").option("header","false").load("Data-20181224/node_information.csv")
     val dfNodeInformation = dfHeaderless.toDF("Id","Year","Title","Authors","Journal","Abstract")
     //load training set
-    val df2 = sparkSession.read.format("csv").option("header","false").load("/home/panos/IdeaProjects/Link_Prediction/Data-20181224/training_set.txt").toDF("info")
+    val df2 = sparkSession.read.format("csv").option("header","false").load("Data-20181224/training_set.txt").toDF("info")
 
     def ProcessDf(NodeInfo: DataFrame,NodeCombination: DataFrame):DataFrame = {
       //break NodeCombination's info into 3 columns(ID1,Id2,label)
@@ -87,32 +104,42 @@ object Classification {
       ).drop("_tmp")
 
       //join the information for every node with id=Id1 rename column names and change the type of column label from string to Int
-      val test=dfComSplit.join(NodeInfo,$"Id1"===$"Id").select($"Id1",$"Id2",$"label",$"fAbstract".as("fAbstract1"),$"Authors".as("Authors1"))
+      val test=dfComSplit.join(NodeInfo,$"Id1"===$"Id").select($"Id1",$"Id2",$"label",$"fAbstract".as("fAbstract1"),
+        $"Authors".as("Authors1"),$"fTitle".as("Title1"),$"Journal".as("Journal1"))
         .withColumn("label",dfComSplit.col("label").cast(IntegerType))
       //join the information for every node with id=Id2 rename column names
-      val lastDf=test.join(NodeInfo,$"Id2"===$"Id").select($"Id1",$"Id2",$"label",$"fAbstract1", $"Authors1",$"fAbstract".as("fAbstract2"),$"Authors".as("Authors2"))
+      val lastDf=test.join(NodeInfo,$"Id2"===$"Id").select($"Id1",$"Id2",$"label",$"fAbstract1", $"Authors1",$"Title1",$"Journal1",
+        $"fAbstract".as("fAbstract2"),$"Authors".as("Authors2"),$"fTitle".as("Title2"),$"Journal".as("Journal2"))
       val dist = udf(sqrdist _ )
       val sAuthor= udf(sameAuthor _)
+      val sJournal = udf(sameJournal _ )
       //produce final dataframe calculate euclidean distance between abstracts and check if there is the same author between 2 articles
       val finalDf=lastDf.withColumn("Dist",dist($"fAbstract1",$"fAbstract2"))
-        .select("Id1","Id2","label","Authors1","Authors2","Dist")
+        .withColumn("DistTitle",dist($"Title1",$"Title2"))
         .withColumn("sameAuthors",sAuthor($"Authors1",$"Authors2"))
-        .select("Id1","Id2","label","Dist","sameAuthors")
         .withColumn("Id",concat_ws(" ",$"Id1",$"Id2"))
-        .select("Id","label","Dist","sameAuthors")
+        .withColumn("sameJournal", sJournal($"Journal1",$"Journal2"))
+        .select("Id","label","Dist","DistTitle","sameAuthors","sameJournal")
       //dataframe's last form is Id,label,Euclidean distance,same authors
       return finalDf
 
     }
 
     //process for the abstact
-    val dfNodeInfoPros=TFIDF(dfNodeInformation,"Abstract","fAbstract",1000).drop("Abstract")
+    val dfNodeInfoProstemp=TFIDF(dfNodeInformation,"Abstract","fAbstract",1000).drop("Abstract")
+    val dfNodeInfoPros=TFIDF(dfNodeInfoProstemp,"Title","fTitle",1000).drop("Title")
     //process the dataframe
     val ProcessedDf=ProcessDf(dfNodeInfoPros,df2)
 
+    /*val normalizer = new Normalizer()
+      .setInputCol("featurestemp")
+      .setOutputCol("features")
+      .setP(1.0)*/
+
     val assembler1 = new VectorAssembler().
-      setInputCols(Array("Dist","sameAuthors")).
+      setInputCols(Array("Dist","DistTitle","sameAuthors","sameJournal")).
       setOutputCol("features")
+
 
     val assembledFinalDf = assembler1.transform(ProcessedDf).select("Id","features","label")
 
@@ -128,11 +155,31 @@ object Classification {
     val evaluator = new MulticlassClassificationEvaluator()
       .setLabelCol("label")
       .setPredictionCol("prediction")
+      .setMetricName("f1")
+    val accuracy = evaluator.evaluate(prediction)
+    println(s"F1 score = ${accuracy}")
+
+
+    /*val lsvc = new LinearSVC()
+      .setMaxIter(10)
+      .setRegParam(0.1)
+
+    // Fit the model
+    val lsvcModel = lsvc.fit(training)
+
+    // Print the coefficients and intercept for linear svc
+    println(s"Coefficients: ${lsvcModel.coefficients} Intercept: ${lsvcModel.intercept}")
+
+    val prediction=lsvcModel.transform(test)
+
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
       .setMetricName("accuracy")
     val accuracy = evaluator.evaluate(prediction)
-    println(s"Test Error = ${accuracy}")
-
+    println(s"Test acc = ${accuracy}")*/
 
 
   }
 }
+
