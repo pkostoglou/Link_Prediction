8180733df5922c6dfa7d91920474d0e6b9599902
Panagiotis Kostoglou <kostoglo@gmail.com>
2019-01-23 23:36:46 +0200

    Complete part 1 working solution for part 2

--- Link_Prediction/src/main/scala/Link_Prediction_Part2.scala
+++ Link_Prediction/src/main/scala/Link_Prediction_Part2.scala
@@ -1,16 +1,48 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark
-import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.evaluation.ClusteringEvaluator
+import org.apache.spark.ml.feature._
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.ml.feature.BucketedRandomProjectionLSH
-import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions.{concat_ws, split, udf}
 import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.storage.StorageLevel
+
 
 
 
 
 object Link_Prediction_Part2 {
 
+  def getSrcId(Year1: Int, Year2: Int,Id1:String,Id2:String):String={
+    if(Year1<Year2){
+      return Id2
+    }else{
+      return Id1
+    }
+  }
+
+  def getDstId(Year1: Int, Year2: Int,Id1:String,Id2:String):String={
+    if(Year1<Year2){
+      return Id1
+    }else{
+      return Id2
+    }
+  }
+
+  def YearDifference(Year1: Int, Year2: Int):Int={
+    return math.abs(Year1 - Year2)
+  }
+
+  //cluster1==cluster2 && Year1-Year2<4
+  def setlabel(cluster1: Int, cluster2: Int,Year1: Int, Year2: Int):Int={
+    if(cluster1==cluster2){
+      return 1
+    }else{
+      return 0
+    }
+  }
 
   def sameAuthor(auth1: String,auth2: String):Int={
     if(auth1==null || auth2 ==null)
@@ -59,34 +91,98 @@ object Link_Prediction_Part2 {
     import sparkSession.implicits._
     import sparkSession.sql
     val dfHeaderless = sparkSession.read.format("csv").option("header", "false").load("Data-20181224/node_information.csv")
-    val dfNodeInformation = dfHeaderless.toDF("Id", "Year", "Title", "Authors", "Journal", "Abstract")
+    val dfNodeInformation = dfHeaderless.toDF("Id", "Year1", "Title", "Authors", "Journal", "Abstract").withColumn("Year",$"Year1".cast(IntegerType))
 
     val dfNodeInfoProstemp=TFIDF(dfNodeInformation,"Title","fTitle",1000).drop("Title")
-    //val dfNodeInfoProstemp=TFIDF(dfNodeInformation,"Abstract","fAbstract",1000).drop("Abstract")
+    val dfNodeInfoPros=TFIDF(dfNodeInfoProstemp,"Abstract","fAbstract",10).drop("Abstract")
 
-    val Array(training, test) = dfNodeInfoProstemp.randomSplit(Array[Double](0.1, 0.9), 18)
+    //val Array(training, test) = dfNodeInfoPros.randomSplit(Array[Double](0.1, 0.9), 18)
 
-    print(training.count())
+    val assemblerHash = new VectorAssembler().
+      setInputCols(Array("fTitle"))
+      .setOutputCol("hashVector")
+    val hashDf=assemblerHash.transform(dfNodeInfoPros)
+
+    val assembler1 = new VectorAssembler().
+      setInputCols(Array("Year")).
+      setOutputCol("featuresTemp")
+
+    val assembledDf = assembler1.transform(hashDf)
+
+    //var i=2
+    //while(i<10) {
+      val kmeans = new KMeans().setK(2).setSeed(1L).setFeaturesCol("featuresTemp")
+      val cluster = kmeans.fit(assembledDf)
+
+      // Make predictions
+      val predictions = cluster.transform(assembledDf)
+
+      //predictions.show()
+
+      // Evaluate clustering by computing Silhouette score
+      val evaluator = new ClusteringEvaluator().setFeaturesCol("featuresTemp")
+
+      val silhouette = evaluator.evaluate(predictions)
+      println(s"Silhouette with squared euclidean distance = $silhouette with i = " )
+      //i=i+1
+    //}
+//val predictions=assembledDf
     val brp = new BucketedRandomProjectionLSH()
-      .setBucketLength(2.0)
-      .setNumHashTables(3)
+      .setBucketLength(1.0)
+      .setNumHashTables(1)
       //.setInputCol("fAbstract")
-      .setInputCol("fTitle")
+      //.setInputCol("fTitle")
+      .setInputCol("hashVector")
       .setOutputCol("hashes")
-    val model = brp.fit(training)
+    val model = brp.fit(predictions)
 
     // Feature Transformation
     println("The hashed dataset where hashed values are stored in the column 'hashes':")
-    val hashedDf=model.transform(training)
+    val hashedDf=model.transform(predictions)
     val sAuthor= udf(sameAuthor _)
-    val c=model.approxSimilarityJoin(hashedDf, hashedDf, 10, "EuclideanDistance")
+    val difYear = udf(YearDifference _ )
+    val setLabel = udf(setlabel _)
+    val getDst = udf(getDstId _)
+    val getSrc = udf(getSrcId _)
+    val c=model.approxSimilarityJoin(hashedDf, hashedDf, 9.0, "EuclideanDistance")
       .select($"datasetA.Id".alias("idA"),
         $"datasetB.Id".alias("idB"),
+        $"datasetA.prediction".alias("clusterA"),
+        $"datasetB.prediction".alias("clusterB"),
         $"EuclideanDistance",
-        $"datasetA.Authors".alias("AuthorsA"),
-        $"datasetB.Authors".alias("AuthorsB")).filter($"idA"=!=$"idB")
-      .withColumn("sameAuthors",sAuthor($"Authors1",$"Authors2")).show()
-}
+        $"datasetA.Year".alias("YearA"),
+        $"datasetB.Year".alias("YearB"))
+        //$"datasetA.Authors".alias("AuthorsA"),
+        //$"datasetB.Authors".alias("AuthorsB"))
+      .filter($"idA"=!=$"idB")
+      .withColumn("label",setLabel($"clusterA",$"clusterB",$"YearA",$"YearB"))
+      .withColumn("IdSrc",getSrc($"YearA",$"YearB",$"IdA",$"IdB"))
+      .withColumn("IdDst",getDst($"YearA",$"YearB",$"IdA",$"IdB"))
+      //.withColumn("IdLabel",concat_ws("\t",$"IdA",$"IdB"))
+    c.persist(StorageLevel.MEMORY_AND_DISK)
+    //c.show()
+
+
+    val myRight=c.filter($"label"===1).count()
+    println("Evaluation begins ")
+    val groundTruth = sparkSession.read.format("csv").option("header", "false").load("Data-20181224/Cit-HepTh.txt").toDF("Id").withColumn("_tmp", split($"Id", "\t")).select(
+      $"_tmp".getItem(0).as("Id1"),
+      $"_tmp".getItem(1).as("Id2")).drop("_tmp")
+    groundTruth.show()
+    val allEdges=groundTruth.count()
+    val evaluation=groundTruth.join(c,($"IdSrc"===$"Id1"&&$"IdDst"===$"Id2"))
+    val setRight=evaluation.filter($"label"===1).count()
+    println(c.count(),evaluation.count)
+    println(setRight,allEdges)
+    val recall = (setRight).toDouble / allEdges
+      println(recall)
+    val precision = (setRight).toDouble/myRight
+    println(precision)
+    println(2*precision*recall/(precision+recall))
+
+
+
+  }
 
 
 
---
+++ Link_Prediction/src/main/scala/graphSolution.scala
@@ -0,0 +1,341 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.sql.types._
+import org.apache.spark.ml.classification.{DecisionTreeClassifier, LinearSVC, LogisticRegression, RandomForestClassifier}
+import org.apache.spark.ml.feature._
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.ml.linalg.SQLDataTypes.VectorType
+import org.apache.spark
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark._
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions.{concat_ws, split, udf}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.regression.DecisionTreeRegressionModel
+import org.apache.spark.ml.regression.DecisionTreeRegressor
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.evaluation.ClusteringEvaluator
+
+import scala.collection.mutable.WrappedArray
+
+object graphSolution {
+
+  def comparison(cluster1: Int, cluster2: Int):Int={
+    if(cluster1==cluster2){
+      return 1
+    }else{
+      return 0
+    }
+
+  }
+
+  def inLinksDifference(links1:Int, links2:Int):Int={
+    return math.abs(links1-links2)
+  }
+
+  def Neighbours(N1: WrappedArray[Long], N2: WrappedArray[Long]):Int={
+    return N1.intersect(N2).length
+  }
+
+  def sqrdist(vec1: Vector , vec2: Vector):Double={
+    val temp1 = vec1.toArray
+    val temp2 = vec2.toArray
+    return math.sqrt(temp1.zip(temp2).map(x=>math.pow(x._1-x._2,2)).reduce((a,b)=>a+b))
+  }
+
+  def cosdist(vec1: Vector , vec2: Vector):Double= {
+    val temp1 = vec1.toArray
+    val temp2 = vec2.toArray
+    val res = temp1.zip(temp2).map(x=>(x._1*x._2,x._1*x._1,x._2*x._2))reduceLeft((a,b)=>(a._1+b._1,a._2+b._2,a._3+b._3))
+    return res._1/(res._2*res._3)
+  }
+
+
+  def jaccard(vec1: Vector , vec2: Vector):Double= {
+    val temp1 = vec1.toArray
+    val temp2 = vec2.toArray
+    return 1-temp1.intersect(temp2).size/temp1.union(temp2).size
+  }
+
+  def sameAuthor(auth1: String,auth2: String):Int={
+    if(auth1==null || auth2 ==null)
+      return 0
+    val temp1=auth1.split(",")
+    val temp2=auth2.split(",")
+    var sameAuthors=0
+    for( i <- 0 until temp1.length){
+      for(j <-0 until temp2.length){
+        if(temp1(i).compareTo(temp2(j))==0){
+          sameAuthors=sameAuthors+1
+        }
+      }
+    }
+    return sameAuthors
+  }
+
+  def sameJournal(Journal1: String,Journal2: String):Int={
+    if(Journal1==null || Journal2 ==null)
+      return 0
+
+    if(Journal1.compareTo(Journal2)==0){
+      return 1
+    }
+    return 0
+  }
+
+  def YearDifference(Year1: Int, Year2: Int):Int={
+    return math.abs(Year1 - Year2)
+  }
+
+  def TFIDF(x: DataFrame,column: String,columnOut: String,numFeatures: Int):DataFrame = {
+    val tokenizer = new Tokenizer().setInputCol(column).setOutputCol("words")
+    val wordsData = tokenizer.transform(x.na.fill(Map(column -> "")))
+
+    val hashingTF = new HashingTF()
+      .setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(numFeatures)
+
+    val featurizedData = hashingTF.transform(wordsData)
+
+    val idf = new IDF().setInputCol("rawFeatures").setOutputCol(columnOut)
+    val idfModel = idf.fit(featurizedData)
+
+    val rescaledData = idfModel.transform(featurizedData)
+    return rescaledData.drop("words").drop("rawFeatures").drop(column)
+  }
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+    val conf = new spark.SparkConf().setAppName("Link Classification Application").setMaster("local[*]")
+    val sc = new spark.SparkContext(conf)
+    val sparkSession = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.some.config.option", "some-value")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    import sparkSession.implicits._
+    import sparkSession.sql
+
+
+    //load node information
+    val dfHeaderless = sparkSession.read.format("csv").option("header", "false").load("Data-20181224/node_information.csv")
+    val dfNodeInformation = dfHeaderless.toDF("Id", "Year", "Title", "Authors", "Journal", "Abstract")
+    //load training set
+    //dfNodeInformation.cache()
+    val df2 = sparkSession.read.format("csv").option("header", "false").load("Data-20181224/training_set.txt").toDF("info")
+
+    val dfComSplit = df2.withColumn("_tmp", split($"info", " ")).select(
+      $"_tmp".getItem(0).as("Id1"),
+      $"_tmp".getItem(1).as("Id2"),
+      $"_tmp".getItem(2).as("label")
+    ).drop("_tmp")//.limit(1000)
+
+    val dfNodeInfoProstemp=TFIDF(dfNodeInformation,"Abstract","fAbstract",20000).drop("Abstract")
+
+
+    val tokenizer = new Tokenizer().setInputCol("Title").setOutputCol("words")
+
+    val word2Vec = new Word2Vec()
+      .setInputCol("words")
+      .setOutputCol("fTitle")
+      .setVectorSize(20)
+      .setMinCount(0)
+    val w=tokenizer.transform(dfNodeInfoProstemp)
+    val model2Vec = word2Vec.fit(w)
+
+    val result = model2Vec.transform(w)
+
+    val Array(train, testing) = dfComSplit.limit(10000).randomSplit(Array[Double](0.8, 0.2), 18)
+    println(train.count(),testing.count())
+
+    val users: RDD[(VertexId, (Int,String, String,Vector,Vector))] = result.rdd.map(x=>(x.getString(0).toLong,(x.getString(1).toInt,x.getString(3),x.getString(4),x.getAs[Vector](5),x.getAs[Vector](7))))
+    val edgesTrain = train.rdd.map(x => Edge(x.getString(0).toLong, x.getString(1).toLong,x.getString(2).toInt))
+    //val edgesTest = testing.rdd.map(x=> Edge(x.getString(0).toLong, x.getString(1).toLong,2))
+    //val edges = edgesTest++edgesTrain
+    val u=VertexRDD(users)
+    val graph = Graph(u,edgesTrain)
+
+
+    val newAttributes: VertexRDD[(Int, String,String,Vector,Vector,Array[Long],Int)] = graph.aggregateMessages[(Int,String, String,Vector,Vector,Array[Long],Int)](
+      triplet => { // Map Function
+        if(triplet.attr==1) {
+          triplet.sendToSrc((triplet.srcAttr._1, triplet.srcAttr._2, triplet.srcAttr._3, triplet.srcAttr._4,triplet.srcAttr._5, Array(triplet.dstId),0))
+          triplet.sendToDst((triplet.dstAttr._1, triplet.dstAttr._2, triplet.dstAttr._3, triplet.dstAttr._4,triplet.dstAttr._5, Array(triplet.srcId),1))
+        }else if(triplet.attr==0){
+          triplet.sendToSrc((triplet.srcAttr._1, triplet.srcAttr._2, triplet.srcAttr._3, triplet.srcAttr._4,triplet.srcAttr._5, Array[Long](),0))
+          triplet.sendToDst((triplet.dstAttr._1, triplet.dstAttr._2, triplet.dstAttr._3, triplet.dstAttr._4,triplet.dstAttr._5, Array[Long](),0))
+        }
+      },
+      (a,b)=>(a._1,a._2,a._3,a._4,a._5,a._6++b._6,a._7+b._7)
+    )
+    val updatedGraph=Graph(newAttributes,edgesTrain)
+    val facts=updatedGraph.triplets
+    val readyRDD= facts.map(x=>(Row(x.srcId,x.srcAttr._1,x.srcAttr._2,x.srcAttr._3,x.srcAttr._4,x.srcAttr._5,x.srcAttr._6,x.srcAttr._7
+      ,x.dstId,x.dstAttr._1,x.dstAttr._2,x.dstAttr._3,x.dstAttr._4,x.dstAttr._5,x.dstAttr._6,x.dstAttr._7,x.attr)))
+    val aStruct = new StructType(Array(StructField("Id1",LongType,nullable = true)
+      ,StructField("Year1",IntegerType,nullable = true)
+      ,StructField("Authors1",StringType,nullable = true)
+      ,StructField("Journal1",StringType,nullable = true)
+      ,StructField("fAbstract1",VectorType,nullable = true)
+      ,StructField("fTitle1",VectorType,nullable = true)
+      ,StructField("Neigh1",ArrayType(LongType,true),nullable = true)
+      ,StructField("InLinks1",IntegerType,nullable = true)
+      ,StructField("Id2",LongType,nullable = true)
+      ,StructField("Year2",IntegerType,nullable = true)
+      ,StructField("Authors2",StringType,nullable = true)
+      ,StructField("Journal2",StringType,nullable = true)
+      ,StructField("fAbstract2",VectorType,nullable = true)
+      ,StructField("fTitle2",VectorType,nullable = true)
+      ,StructField("Neigh2",ArrayType(LongType,true),nullable = true)
+      ,StructField("InLinks2",IntegerType,nullable = true)
+      ,StructField("label",IntegerType,nullable = true)))
+      //.toDF("Id1","Year1","Author1","Journal1","fAbstract1","fTitle1"
+        //,"Id2","Year2","Author2","Journal2","fAbstract2","fTitle2")
+    val readyDf = sparkSession.createDataFrame(readyRDD,aStruct)
+
+    val jacdist= udf(jaccard _ )
+    val sqdist = udf(sqrdist _ )
+    val cosinedist = udf(cosdist _ )
+    val clusterComparison = udf(comparison _ )
+    val sAuthor= udf(sameAuthor _)
+    val sJournal = udf(sameJournal _ )
+    val difYear = udf(YearDifference _ )
+    val sNeighbours = udf(Neighbours _ )
+    val inLinksDif= udf(inLinksDifference _)
+    //produce final dataframe calculate euclidean distance between abstracts and check if there is the same author between 2 articles
+    val finalDf=readyDf.withColumn("Dist",cosinedist($"fAbstract1",$"fAbstract2"))
+      .withColumn("DistTitle",cosinedist($"fTitle1",$"fTitle2"))
+      .withColumn("YearDifference",difYear($"Year1",$"Year2"))
+      //.withColumn("sameAuthors",sAuthor($"Authors1",$"Authors2"))
+      .withColumn("Id",concat_ws(" ",$"Id1",$"Id2"))
+      //.withColumn("sameJournal", sJournal($"Journal1",$"Journal2"))
+      .withColumn("sameNeighbours",sNeighbours($"Neigh1",$"Neigh2"))
+      .withColumn("LinksDifference",inLinksDif($"InLinks2",$"InLinks1"))
+      .select("Id","label","YearDifference","Dist","DistTitle","sameNeighbours","LinksDifference")
+
+    finalDf.show()
+    val assembler1 = new VectorAssembler().
+      setInputCols(Array("Dist","sameNeighbours","YearDifference","DistTitle","LinksDifference")).
+      setOutputCol("featuresTemp")
+
+    //"Dist","DistTitle","sameAuthors","sameJournal","YearDifference","sameNeighbours"
+
+    val assembledFinalDf = assembler1.transform(finalDf).select("Id","featuresTemp","label")
+
+    import org.apache.spark.ml.feature.Normalizer
+    val scaler = new Normalizer()
+      .setInputCol("featuresTemp")
+      .setOutputCol("features")
+      .setP(1.0)
+
+    val scaledData = scaler.transform(assembledFinalDf)
+
+   /* MinMaxScaler
+    val scaler = new MinMaxScaler()
+      .setInputCol("featuresTemp")
+      .setOutputCol("features")
+
+    // Compute summary statistics and generate MinMaxScalerModel
+    val scalerModel = scaler.fit(assembledFinalDf)
+
+    // rescale each feature to range [min, max].
+    val scaledData = scalerModel.transform(assembledFinalDf)*/
+
+    //val Array(training, test) = scaledData.randomSplit(Array[Double](0.8, 0.2), 18)
+
+
+
+    val rf = new RandomForestClassifier()
+      .setLabelCol("label")
+      .setFeaturesCol("features")
+      .setNumTrees(10)
+
+    val trained=rf.fit(scaledData)
+
+    val edgesTest = testing.rdd.map(x=> Edge(x.getString(0).toLong, x.getString(1).toLong,2)) ++ edgesTrain
+
+    val graphTest = Graph(u,edgesTest)
+
+
+    val newAttributesTest: VertexRDD[(Int, String,String,Vector,Vector,Array[Long],Int)] = graphTest.aggregateMessages[(Int,String, String,Vector,Vector,Array[Long],Int)](
+      triplet => { // Map Function
+        if(triplet.attr==1) {
+          triplet.sendToSrc((triplet.srcAttr._1, triplet.srcAttr._2, triplet.srcAttr._3, triplet.srcAttr._4,triplet.srcAttr._5, Array(triplet.dstId),0))
+          triplet.sendToDst((triplet.dstAttr._1, triplet.dstAttr._2, triplet.dstAttr._3, triplet.dstAttr._4,triplet.dstAttr._5, Array(triplet.srcId),1))
+        }else if(triplet.attr==0 || triplet.attr==2 ){
+          triplet.sendToSrc((triplet.srcAttr._1, triplet.srcAttr._2, triplet.srcAttr._3, triplet.srcAttr._4,triplet.srcAttr._5, Array[Long](),0))
+          triplet.sendToDst((triplet.dstAttr._1, triplet.dstAttr._2, triplet.dstAttr._3, triplet.dstAttr._4,triplet.dstAttr._5, Array[Long](),0))
+        }
+      },
+      (a,b)=>(a._1,a._2,a._3,a._4,a._5,a._6++b._6,a._7+b._7)
+    )
+    val updatedGraphTest=Graph(newAttributesTest,edgesTest)
+    val factsTest=updatedGraphTest.triplets
+    val readyRDDTest= factsTest.map(x=>(Row(x.srcId,x.srcAttr._1,x.srcAttr._2,x.srcAttr._3,x.srcAttr._4,x.srcAttr._5,x.srcAttr._6,x.srcAttr._7
+      ,x.dstId,x.dstAttr._1,x.dstAttr._2,x.dstAttr._3,x.dstAttr._4,x.dstAttr._5,x.dstAttr._6,x.dstAttr._7,x.attr)))
+    val aStructTest = new StructType(Array(StructField("Id1",LongType,nullable = true)
+      ,StructField("Year1",IntegerType,nullable = true)
+      ,StructField("Authors1",StringType,nullable = true)
+      ,StructField("Journal1",StringType,nullable = true)
+      ,StructField("fAbstract1",VectorType,nullable = true)
+      ,StructField("fTitle1",VectorType,nullable = true)
+      ,StructField("Neigh1",ArrayType(LongType,true),nullable = true)
+      ,StructField("InLinks1",IntegerType,nullable = true)
+      ,StructField("Id2",LongType,nullable = true)
+      ,StructField("Year2",IntegerType,nullable = true)
+      ,StructField("Authors2",StringType,nullable = true)
+      ,StructField("Journal2",StringType,nullable = true)
+      ,StructField("fAbstract2",VectorType,nullable = true)
+      ,StructField("fTitle2",VectorType,nullable = true)
+      ,StructField("Neigh2",ArrayType(LongType,true),nullable = true)
+      ,StructField("InLinks2",IntegerType,nullable = true)
+      ,StructField("label",IntegerType,nullable = true)))
+    //.toDF("Id1","Year1","Author1","Journal1","fAbstract1","fTitle1"
+    //,"Id2","Year2","Author2","Journal2","fAbstract2","fTitle2")
+    val readyDfTest = sparkSession.createDataFrame(readyRDDTest,aStructTest).filter($"label"===2)
+
+    val finalDfTest=readyDfTest.withColumn("Dist",cosinedist($"fAbstract1",$"fAbstract2"))
+      .withColumn("DistTitle",cosinedist($"fTitle1",$"fTitle2"))
+      .withColumn("YearDifference",difYear($"Year1",$"Year2"))
+      //.withColumn("sameAuthors",sAuthor($"Authors1",$"Authors2"))
+      .withColumn("Id",concat_ws(" ",$"Id1",$"Id2"))
+      //.withColumn("sameJournal", sJournal($"Journal1",$"Journal2"))
+      .withColumn("sameNeighbours",sNeighbours($"Neigh1",$"Neigh2"))
+      .withColumn("LinksDifference",inLinksDif($"InLinks2",$"InLinks1"))
+      .select("Id","label","Dist","YearDifference","DistTitle","sameNeighbours","LinksDifference")
+
+
+    //"Dist","DistTitle","sameAuthors","sameJournal","YearDifference","sameNeighbours"
+
+    val assembledFinalDfTest = assembler1.transform(finalDfTest).select("Id","featuresTemp","label")
+
+    val scaledDataTest = scaler.transform(assembledFinalDfTest)
+
+    val prediction=trained.transform(scaledDataTest)
+    val ntesting=testing
+      .withColumn("Idt",concat_ws(" ",$"Id1",$"Id2"))
+      .withColumn("trueLabel",testing.col("label").cast(IntegerType))
+      .select($"Idt",$"trueLabel")
+    val fPrediction= prediction.join(ntesting,$"Id"===$"Idt")
+    fPrediction.show()
+    //decision tree model
+    /*val dt = new DecisionTreeClassifier()
+      .setLabelCol("label")
+      .setFeaturesCol("features")
+    val trained=dt.fit(training)
+    val prediction=trained.transform(test)*/
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("trueLabel")
+      .setPredictionCol("prediction")
+      .setMetricName("f1")
+    val accuracy = evaluator.evaluate(fPrediction)
+    println(s"F1 score = ${accuracy}")
+
+  }
+}
+
